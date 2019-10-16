48182f926c18e577ba547ff67b8a153b4c30c618
Gabriel Polastrini <bielp07@gmail.com>
2019-10-16 09:34:41 -0300

    Render input value on screen.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {text: ''};
+  };
+  
+  textChange(event) {
+    this.setState({text: event.target.value});
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>WEIGHT</h1>
+          <p>Generate text with different weights in real time!</p>
+          <input
+            type="text"
+            onKeyUp={this.textChange.bind(this)}
+          />
+          <p className="light">{this.state.text}</p>
+        </header> 
+        <footer>
+          <p>Made with ♥  by <a href="https://gabrielpolastrini.com" target="_blank" rel="noopener noreferrer">Gabriel Polastrini</a></p>
+        </footer>
+      </div>
+    );
+  }
+};
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
